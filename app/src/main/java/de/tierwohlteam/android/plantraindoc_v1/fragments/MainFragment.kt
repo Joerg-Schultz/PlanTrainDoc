68b5222ef0987e2dcf2c2c0ab123a4a7a6149769
Joerg-Schultz <JoergSky@online.de>
2021-05-19 08:43:44 +0200

    Initial commit

---
+++ app/src/main/java/de/tierwohlteam/android/plantraindoc_v1/fragments/MainFragment.kt
@@ -0,0 +1,33 @@
+package de.tierwohlteam.android.plantraindoc_v1.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import de.tierwohlteam.android.plantraindoc_v1.R
+import de.tierwohlteam.android.plantraindoc_v1.viewmodels.MainViewModel
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
