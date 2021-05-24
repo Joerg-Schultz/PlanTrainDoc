b7629d9be045cf243dddbfab6026880c5d79df33
Joerg-Schultz <JoergSky@online.de>
2021-05-24 12:34:10 +0200

    test insert and get user

--- app/src/androidTest/java/de/tierwohlteam/android/plantraindoc_v1/daos/UserDaoTest.kt
+++ app/src/androidTest/java/de/tierwohlteam/android/plantraindoc_v1/daos/UserDaoTest.kt
@@ -5,6 +5,7 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.benasher44.uuid.uuid4
+import com.google.common.truth.Truth.assertThat
 import de.tierwohlteam.android.plantraindoc_v1.models.User
 import de.tierwohlteam.android.plantraindoc_v1.repositories.PTDdb
 import kotlinx.coroutines.runBlocking
@@ -32,12 +33,14 @@ class UserDaoTest {
     }
     @Test
     @Throws(Exception::class)
-    fun insertUser() = runBlocking {
+    fun insertAndGetUser() = runBlocking {
         val userID = uuid4()
         val user = User(id = userID, name = "Test User", email = "testuser@mail.de", password = "123", role = "standard")
         userDao.insert(user)
-        //val dbUser = userDao.getByID(userID)
+        val dbUser = userDao.getByID(userID)
+        assertThat(dbUser).isEqualTo(user)
     }
+
     @After
     @Throws(IOException::class)
     fun closeDb() {
--- app/src/main/java/de/tierwohlteam/android/plantraindoc_v1/daos/UserDao.kt
+++ app/src/main/java/de/tierwohlteam/android/plantraindoc_v1/daos/UserDao.kt
@@ -3,11 +3,16 @@ package de.tierwohlteam.android.plantraindoc_v1.daos
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.benasher44.uuid.Uuid
 import de.tierwohlteam.android.plantraindoc_v1.models.User
 
 @Dao
 interface UserDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(user: User)
+
+    @Query("SELECT * from users where id = :userID")
+    fun getByID(userID: Uuid): User
 
 }
