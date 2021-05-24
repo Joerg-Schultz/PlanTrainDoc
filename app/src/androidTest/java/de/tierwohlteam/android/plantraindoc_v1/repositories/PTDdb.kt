c81d28df3200c76aceeed1562e2bc7b35f6417c5
Joerg-Schultz <JoergSky@online.de>
2021-05-24 07:01:48 +0200

    Skeleton for testing DB

---
+++ app/src/androidTest/java/de/tierwohlteam/android/plantraindoc_v1/repositories/PTDdb.kt
@@ -0,0 +1,19 @@
+package de.tierwohlteam.android.plantraindoc_v1.repositories
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Test initiation of Room database
+ * https://medium.com/androiddevelopers/using-and-testing-room-kotlin-apis-4d69438f9334
+ *
+ */
+
+@RunWith(AndroidJUnit4::class)
+class PTDdbTest{
+    @Test
+    internal fun connectDBTest(){
+        assert(true)
+    }
+}
