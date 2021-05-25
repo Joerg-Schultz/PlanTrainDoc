cf3a2441ecadff500bbb2709dca2083d2d0f7fd8
Joerg-Schultz <JoergSky@online.de>
2021-05-25 17:38:02 +0200

    Goal with dependencies and parent

--- app/src/main/java/de/tierwohlteam/android/plantraindoc_v1/models/Goal.kt
+++ app/src/main/java/de/tierwohlteam/android/plantraindoc_v1/models/Goal.kt
@@ -1,10 +1,15 @@
+@file:UseSerializers(
+    UUIDAsStringSerializer::class
+)
 package de.tierwohlteam.android.plantraindoc_v1.models
 
+import androidx.room.*
 import com.benasher44.uuid.Uuid
 import com.benasher44.uuid.uuid4
 import kotlinx.datetime.*
 import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.UseSerializers
 
 
 /**
@@ -21,20 +26,33 @@ import kotlinx.serialization.Serializable
  * @property[changed] timestamp for last modification
  * @property[user] to whom this goal belongs
  */
-
+@Entity(tableName = "goals",
+    foreignKeys = arrayOf(
+        ForeignKey(
+            entity = User::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("userID")
+        ),
+        ForeignKey(
+            entity = Goal::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("parents")
+        )
+    ),
+    indices = [Index(value = arrayOf("userID"))]
+)
 @Serializable
 data class Goal(
-    @Serializable(UUIDAsStringSerializer::class)
+    @PrimaryKey
     var id: Uuid = uuid4(),
     var goal: String,
     var description: String = "",
-    var parents: List<Goal> = emptyList(), //I am currently using only the first element. Tree, no DAG
-    var dependencies: List<Goal> = emptyList(),
+    var parents: Uuid? = null, //implemented as list in KTor, but only first element is used
     var position: Int = 0, // ranking in relation so sisters
     var status: String = statusNew, //"in progress", "finished", "stopped"
     var created: LocalDateTime = Clock.System.now().toLocalDateTime(TimeZone.UTC),
     var changed: LocalDateTime = Clock.System.now().toLocalDateTime(TimeZone.UTC),
-    val user: User
+    val userID: Uuid
 ) {
     companion object {
         const val path = "/goals"
@@ -44,3 +62,18 @@ data class Goal(
         const val statusStopped = "stopped"
     }
 }
+
+data class GoalWithParentAndDependencies(
+    @Embedded
+    val goal: Goal,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "parents"
+    )
+    val parent: Goal? = null,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "id"
+    )
+    val dependencies: List<Goal> = emptyList()
+)
---
+++ app/src/test/java/de/tierwohlteam/android/plantraindoc_v1/models/GoalTest.kt
@@ -0,0 +1,41 @@
+package de.tierwohlteam.android.plantraindoc_v1.models
+
+import com.benasher44.uuid.uuid4
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class GoalTest {
+    @Test
+    internal fun goalTest(){
+        val goalID = uuid4()
+        val userID = uuid4() //add user object here??
+        val goal = Goal(id = goalID, goal = "Sit", userID = userID)
+        assertThat(goal.status).isEqualTo(Goal.statusNew)
+        assertThat(goal.parents).isNull()
+    }
+
+    @Test
+    internal fun goalParentTest(){
+        val parentGoalID = uuid4()
+        val childGoalID = uuid4()
+        val userID = uuid4() //add user object here??
+        val parentGoal = Goal(id = parentGoalID, goal = "Sit", userID = userID)
+        val childGoal = Goal(id = childGoalID, goal = "Sit 2 min", userID = userID, )
+        val goalWithParent = GoalWithParentAndDependencies(goal = childGoal, parent = parentGoal)
+        assertThat(goalWithParent.parent).isEqualTo(parentGoal)
+        assertThat(goalWithParent.dependencies).isEmpty()
+    }
+
+    @Test
+    internal fun goalDependenciesTest(){
+        val goalID = uuid4()
+        val dependencyGoalID = uuid4()
+        val userID = uuid4() //add user object here??
+        val goal = Goal(id = goalID, goal = "Sit", userID = userID)
+        val dependencyGoal = Goal(id = dependencyGoalID, goal = "Down 2 min", userID = userID )
+        val goalWithDependency = GoalWithParentAndDependencies(goal = goal, dependencies = listOf(dependencyGoal))
+        assertThat(goal.parents).isNull()
+        assertThat(goalWithDependency.dependencies).contains(dependencyGoal)
+    }
+
+}
