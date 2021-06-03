44b1ef230e27293c327844a0af99f41c68ab0d84
Joerg-Schultz <JoergSky@online.de>
2021-06-03 16:57:14 +0200

    Added skeleton for User Profile

---
+++ app/src/main/java/de/tierwohlteam/android/plantraindoc_v1/viewmodels/UserProfileViewModel.kt
@@ -0,0 +1,7 @@
+package de.tierwohlteam.android.plantraindoc_v1.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class UserProfileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
