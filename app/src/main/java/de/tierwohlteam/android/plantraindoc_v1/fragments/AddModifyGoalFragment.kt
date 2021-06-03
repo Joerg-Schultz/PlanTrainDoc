1835a9a707e8afa9fbbdc37d1b4b0d82b1319b18
Joerg-Schultz <JoergSky@online.de>
2021-06-03 08:44:46 +0200

    Get infos from UI into Fragment and send to viewModel

--- app/src/main/java/de/tierwohlteam/android/plantraindoc_v1/fragments/AddModifyGoalFragment.kt
+++ app/src/main/java/de/tierwohlteam/android/plantraindoc_v1/fragments/AddModifyGoalFragment.kt
@@ -1,33 +1,68 @@
 package de.tierwohlteam.android.plantraindoc_v1.fragments
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import de.tierwohlteam.android.plantraindoc_v1.viewmodels.AddModifyGoalViewModel
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import dagger.hilt.android.AndroidEntryPoint
 import de.tierwohlteam.android.plantraindoc_v1.R
+import de.tierwohlteam.android.plantraindoc_v1.databinding.AddModifyGoalFragmentBinding
+import de.tierwohlteam.android.plantraindoc_v1.models.Goal
+import de.tierwohlteam.android.plantraindoc_v1.viewmodels.AddModifyGoalViewModel
 
-class AddModifyGoalFragment : Fragment() {
+@AndroidEntryPoint
+class AddModifyGoalFragment : Fragment(R.layout.add_modify_goal_fragment) {
+    private val viewModel: AddModifyGoalViewModel by viewModels()
+    private var _binding: AddModifyGoalFragmentBinding? = null
+    private val binding get() = _binding!!
 
-    companion object {
-        fun newInstance() = AddModifyGoalFragment()
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = AddModifyGoalFragmentBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
-    private lateinit var viewModel: AddModifyGoalViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (viewModel.goal != null) fillFields(viewModel.goal!!)
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.add_modify_goal_fragment, container, false)
+        binding.buttonSavegoal.setOnClickListener {
+            //TODO Add Observer for insert Status
+            // https://github.com/philipplackner/ShoppingListTestingYT/blob/TestItemDeletion/app/src/main/java/com/androiddevs/shoppinglisttestingyt/ui/AddShoppingItemFragment.kt
+            viewModel.saveGoal(goal = binding.tiGoal.text.toString(),
+                description = binding.tiDescription.text.toString(),
+                status = selectedStatus()
+            )
+            view.findNavController().popBackStack()
+        }
+        binding.buttonCancel.setOnClickListener {
+            //TODO move back
+            view.findNavController().popBackStack()
+        }
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(AddModifyGoalViewModel::class.java)
-        // TODO: Use the ViewModel
+    private fun selectedStatus() : String? {
+        val selectedRadio = binding.radioGroupStatus.checkedRadioButtonId
+        return when(selectedRadio){
+            R.id.radioButton_new -> Goal.statusNew
+            R.id.radioButton_inprogress -> Goal.statusInProgress
+            R.id.radioButton_stopped -> Goal.statusStopped
+            R.id.radioButton_done -> Goal.statusFinished
+            else -> null
+        }
     }
-
+    private fun fillFields(goal:Goal){
+        binding.tiGoal.setText(goal.goal)
+        binding.tiDescription.setText(goal.description)
+        when(goal.status){
+            Goal.statusNew -> binding.radioButtonNew.isChecked
+            Goal.statusInProgress -> binding.radioButtonInprogress.isChecked
+            Goal.statusStopped -> binding.radioButtonStopped.isChecked
+            Goal.statusFinished -> binding.radioButtonDone.isChecked
+        }
+    }
 }
--- app/src/main/java/de/tierwohlteam/android/plantraindoc_v1/viewmodels/AddModifyGoalViewModel.kt
+++ app/src/main/java/de/tierwohlteam/android/plantraindoc_v1/viewmodels/AddModifyGoalViewModel.kt
@@ -1,7 +1,21 @@
 package de.tierwohlteam.android.plantraindoc_v1.viewmodels
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import de.tierwohlteam.android.plantraindoc_v1.models.Goal
+import de.tierwohlteam.android.plantraindoc_v1.repositories.PTDRepository
+import javax.inject.Inject
 
-class AddModifyGoalViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class AddModifyGoalViewModel @Inject constructor(
+    private val repository: PTDRepository
+): ViewModel() {
+    //TODO Add FlowState(?) for status of insert
+    // https://github.com/philipplackner/ShoppingListTestingYT/blob/TestItemDeletion/app/src/main/java/com/androiddevs/shoppinglisttestingyt/ui/ShoppingViewModel.kt
+    fun saveGoal(goal: String?, description: String?, status: String?) {
+
+    }
+
+    val goal: Goal? = null
+
 }
