309fef90d456537957fe2bbee1866b94cfd97d8c
Joerg-Schultz <JoergSky@online.de>
2021-06-09 11:20:08 +0200

    Skeleton for show training

---
+++ app/src/main/java/de/tierwohlteam/android/plantraindoc_v1/fragments/ShowTrainingFragment.kt
@@ -0,0 +1,8 @@
+package de.tierwohlteam.android.plantraindoc_v1.fragments
+
+import androidx.fragment.app.Fragment
+import de.tierwohlteam.android.plantraindoc_v1.R
+
+class ShowTrainingFragment : Fragment(R.layout.show_training_fragment) {
+
+}
---
+++ app/src/main/java/de/tierwohlteam/android/plantraindoc_v1/viewmodels/TrainingViewModel.kt
@@ -0,0 +1,7 @@
+package de.tierwohlteam.android.plantraindoc_v1.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class TrainingViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
