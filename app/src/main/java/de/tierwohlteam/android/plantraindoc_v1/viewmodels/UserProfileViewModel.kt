d94ddecd90ef8e875282b062f605fe3481a43203
Joerg-Schultz <JoergSky@online.de>
2021-06-23 14:10:41 +0200

    Deleted User ProfileFragment

--- app/src/main/java/de/tierwohlteam/android/plantraindoc_v1/viewmodels/UserProfileViewModel.kt
+++
@@ -1,7 +0,0 @@
-package de.tierwohlteam.android.plantraindoc_v1.viewmodels
-
-import androidx.lifecycle.ViewModel
-
-class UserProfileViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
