5b245b3b517bc9896b64689c825b045540402297
Joerg-Schultz <JoergSky@online.de>
2021-06-24 07:19:58 +0200

    added statistics

---
+++ app/src/main/java/de/tierwohlteam/android/plantraindoc_v1/viewmodels/StatisticsViewModel.kt
@@ -0,0 +1,7 @@
+package de.tierwohlteam.android.plantraindoc_v1.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class StatisticsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
