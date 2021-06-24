a899e832b4f1d841d21a12049b6b0295dcb7a6fd
Joerg-Schultz <JoergSky@online.de>
2021-06-24 08:33:03 +0200

    prepared fragment with goal

--- app/src/main/java/de/tierwohlteam/android/plantraindoc_v1/fragments/StatisticsFragment.kt
+++ app/src/main/java/de/tierwohlteam/android/plantraindoc_v1/fragments/StatisticsFragment.kt
@@ -6,28 +6,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import dagger.hilt.android.AndroidEntryPoint
 import de.tierwohlteam.android.plantraindoc_v1.R
+import de.tierwohlteam.android.plantraindoc_v1.databinding.ShowTrainingFragmentBinding
+import de.tierwohlteam.android.plantraindoc_v1.databinding.StatisticsFragmentBinding
+import de.tierwohlteam.android.plantraindoc_v1.models.Goal
+import de.tierwohlteam.android.plantraindoc_v1.viewmodels.GoalViewModel
 import de.tierwohlteam.android.plantraindoc_v1.viewmodels.StatisticsViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
-class StatisticsFragment : Fragment() {
+@ExperimentalCoroutinesApi
+@AndroidEntryPoint
+class StatisticsFragment : Fragment(R.layout.statistics_fragment) {
+    private val goalViewModel: GoalViewModel by activityViewModels()
+    private val statsViewModel: StatisticsViewModel by activityViewModels()
 
-    companion object {
-        fun newInstance() = StatisticsFragment()
+    private var _binding: StatisticsFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    private var goal: Goal? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = StatisticsFragmentBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
-    private lateinit var viewModel: StatisticsViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        goal = goalViewModel.selectedGoal.value?.goal
+        binding.tvStatsforgoal.text = goal?.goal
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.statistics_fragment, container, false)
     }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(StatisticsViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
 }
