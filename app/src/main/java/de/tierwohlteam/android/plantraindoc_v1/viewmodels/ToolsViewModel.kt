180fcd012d77cc56878ee74902373fcc8fbed8be
Joerg-Schultz <joergsky@online.de>
2022-01-22 07:30:23 +0100

    Dog on Mat Stateflow

--- app/src/main/java/de/tierwohlteam/android/plantraindoc_v1/viewmodels/ToolsViewModel.kt
+++ app/src/main/java/de/tierwohlteam/android/plantraindoc_v1/viewmodels/ToolsViewModel.kt
@@ -6,7 +6,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import de.tierwohlteam.android.plantraindoc_v1.models.blueToothTools.LightGate
+import de.tierwohlteam.android.plantraindoc_v1.models.blueToothTools.VisionMat
 import de.tierwohlteam.android.plantraindoc_v1.others.Constants.KEY_USE_LIGHT_GATE
+import de.tierwohlteam.android.plantraindoc_v1.others.Constants.KEY_USE_VISION_MAT
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
@@ -22,12 +24,24 @@ class ToolsViewModel @Inject constructor(
     private val _cooperationLightGate: MutableStateFlow<Boolean> = MutableStateFlow(value = false)
     val cooperationLightGate: StateFlow<Boolean> = _cooperationLightGate
 
+    private val _dogOnMat: MutableStateFlow<Boolean> = MutableStateFlow(value = false)
+    val dogOnMat: StateFlow<Boolean> = _dogOnMat
+
     init {
         val useLightGate = sharedPrefs.getBoolean(KEY_USE_LIGHT_GATE, false)
         if (useLightGate) {
             viewModelScope.launch {
                 LightGate.cooperation.collect {
                     _cooperationLightGate.value = it
+                }
+            }
+        }
+
+        val useVisionMat = sharedPrefs.getBoolean(KEY_USE_VISION_MAT, false)
+        if (useVisionMat) {
+            viewModelScope.launch {
+                VisionMat.dogOnMat.collect {
+                    _dogOnMat.value = it
                 }
             }
         }
